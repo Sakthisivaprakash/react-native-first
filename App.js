670ef895c75abfbe83bd848a574089ceb9b6b082
Sakthisivaprakash <sakthisivaprakash@gmail.com>
2022-09-04 00:43:35 +0530

    Initial commit of react native

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={{ ...styles.titleText, fontWeight: "bold" }}>
+          The first app of Sakthi
+        </Text>
+      </View>
+      <Text style={styles.titleText}>Hello World!!!</Text>
+      <Button title="Tap me !" />
     </View>
   );
 }
@@ -13,8 +17,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleText: {
+    margin: 15,
+    borderWidth: 1,
+    borderColor: "blue",
+    padding: 15,
   },
 });
