8410c6370cbd143879d5c71f8802ca00f795786e
Sakthisivaprakash <sakthisivaprakash@gmail.com>
2022-09-04 13:24:50 +0530

    basic structure

--- App.js
+++ App.js
@@ -1,30 +1,32 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={{ ...styles.titleText, fontWeight: "bold" }}>
-          The first app of Sakthi
-        </Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button title="Add Goal" />
       </View>
-      <Text style={styles.titleText}>Hello World!!!</Text>
-      <Button title="Tap me !" />
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 30,
   },
-  titleText: {
-    margin: 15,
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
     borderWidth: 1,
-    borderColor: "blue",
-    padding: 15,
+    borderColor: "#ccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8
   },
 });
